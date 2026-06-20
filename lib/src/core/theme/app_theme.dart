81aa8853bdde62a5360ae04cbd6a8d7b1a3f198d
Joep Beusenberg <git@bepost.nl>
2026-06-20 12:29:15 +0400

    feat: add icons

--- lib/src/core/theme/app_theme.dart
+++ lib/src/core/theme/app_theme.dart
@@ -4,7 +4,9 @@ import 'package:flutter/material.dart';
 abstract final class AppTheme {
   static ThemeData get light => ThemeData(
     useMaterial3: true,
-    colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: Color.fromARGB(0xFF, 0xAA, 0x00, 0xFF),
+    ),
   );
 
   static ThemeData get dark => ThemeData(
